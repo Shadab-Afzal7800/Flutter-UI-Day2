9360f1209d7bb5f0fd87ebd807af9cfbbe830c1e
Shadab-Afzal7800 <shadabafzal7800@gmail.com>
2024-02-28 15:04:20 +0530

    modifies

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_ui_day2/main.dart';
+import 'package:flutter_ui_day2/pages/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(HomeScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
